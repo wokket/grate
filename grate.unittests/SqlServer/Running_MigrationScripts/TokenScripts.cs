caafff06a1e761a565ed2ceb18b7e55c43e884e7
Wokket <github@wokket.com>
2022-09-07 08:09:52 +1000

    bug #232: Added unit test repro for failure

--- grate.unittests/SqlServer/Running_MigrationScripts/TokenScripts.cs
+++ grate.unittests/SqlServer/Running_MigrationScripts/TokenScripts.cs
@@ -1,5 +1,12 @@
-﻿using grate.unittests.TestInfrastructure;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Dapper;
+using FluentAssertions;
+using grate.Configuration;
+using grate.unittests.TestInfrastructure;
 using NUnit.Framework;
+using static grate.Configuration.KnownFolderKeys;
 
 namespace grate.unittests.SqlServer.Running_MigrationScripts;
 
@@ -8,4 +15,41 @@ namespace grate.unittests.SqlServer.Running_MigrationScripts;
 public class TokenScripts : Generic.Running_MigrationScripts.TokenScripts
 {
     protected override IGrateTestContext Context => GrateTestContext.SqlServer;
+
+    private const string Bug232Sql = @"
+ALTER DATABASE {{DatabaseName}} SET ALLOW_SNAPSHOT_ISOLATION ON;
+ALTER DATABASE {{DatabaseName}} SET READ_COMMITTED_SNAPSHOT ON";
+
+    [Test]
+    public async Task Bug232_Timeout_14_Regression()
+    {
+        // V1.4 regressed something, trying to repro
+
+        var db = TestConfig.RandomDatabase();
+
+        var parent = CreateRandomTempDirectory();
+        var knownFolders = FoldersConfiguration.Default(null);
+        var path = new DirectoryInfo(Path.Combine(parent.ToString(), knownFolders[RunAfterCreateDatabase]?.Path ?? throw new Exception("Config Fail")));
+
+        WriteSql(path, "token.sql", Bug232Sql);
+
+        await using (var migrator = Context.GetMigrator(db, parent, knownFolders))
+        {
+            await migrator.Migrate();
+        }
+
+        // Now drop it and do it again
+        var config = Context.GetConfiguration(db, parent, knownFolders) with
+        {
+            Drop = true
+        };
+
+        await using (var migrator = Context.GetMigrator(config))
+        {
+            await migrator.Migrate();
+        }
+
+    }
+
+
 }
