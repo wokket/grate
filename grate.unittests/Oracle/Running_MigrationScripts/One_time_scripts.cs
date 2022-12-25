06066dcf8360b21df998b9e3049842cfd1038b28
Tim Thompson <github@wokket.com>
2022-12-25 09:25:44 +0100

    bug #283: Added attempt at a repro example, but it's passing?

--- grate.unittests/Oracle/Running_MigrationScripts/One_time_scripts.cs
+++ grate.unittests/Oracle/Running_MigrationScripts/One_time_scripts.cs
@@ -1,5 +1,13 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+using grate.Configuration;
+using grate.Migration;
 using grate.unittests.TestInfrastructure;
 using NUnit.Framework;
+
+using static grate.Configuration.KnownFolderKeys;
 
 namespace grate.unittests.Oracle.Running_MigrationScripts;
 
@@ -12,4 +20,29 @@ public class One_time_scripts: Generic.Running_MigrationScripts.One_time_scripts
 
     protected override string CreateView1 => base.CreateView1 + " FROM DUAL";
     protected override string CreateView2 => base.CreateView2 + " FROM DUAL";
+
+    [Test]
+    public async Task Bug283_Syntax_Error_On_Create()
+    {
+        var db = TestConfig.RandomDatabase();
+
+        var parent = CreateRandomTempDirectory();
+        var knownFolders = FoldersConfiguration.Default(null);
+        var path = new DirectoryInfo(Path.Combine(parent.ToString(), knownFolders[Up]?.Path ?? throw new Exception("Config Fail")));
+        var config = Context.GetConfiguration(db, parent, knownFolders);
+
+        WriteSql(path, "script0001.tables.sql", Bug283ReproSql);
+
+        await using var migrator = Context.GetMigrator(config);
+        await migrator.Migrate();
+    }
+
+    private static string Bug283ReproSql => @"
+CREATE TABLE actor (
+actor_id numeric NOT NULL ,
+first_name VARCHAR(45) NOT NULL,
+last_name VARCHAR(45) NOT NULL,
+last_update DATE NOT NULL,
+CONSTRAINT pk_actor PRIMARY KEY (actor_id)
+);";
 }
